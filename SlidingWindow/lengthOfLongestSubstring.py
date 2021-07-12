941b98a7dc9626afe44cf2a4f5563a07469a5f97
Andrew Park <cpark128@umd.edu>
2021-07-12 14:16:37 -0400

    7/12

--- SlidingWindow/lengthOfLongestSubstring.py
+++ SlidingWindow/lengthOfLongestSubstring.py
@@ -6,6 +6,7 @@ Given a string s, find the length of the longest substring without repeating cha
 
 # Dynamic window size problem
 def lengthOfLongestSubstring(s: str) -> int:
+    # My solution
     # Check if string is empty
     if not s:
         return 0
@@ -34,6 +35,33 @@ def lengthOfLongestSubstring(s: str) -> int:
             maxCount = currCount
     
     return maxCount
+
+
+def lengthOfLongestSubstringSimple(s: str) -> int:
+    # Simpler solution from LC discussion post
+    seen = {}
+    l = 0
+    output = 0
+    for r in range(len(s)):
+        if s[r] not in seen:
+            """
+            If s[r] not in seen, we can keep increasing the window size by moving right pointer
+            """
+            output = max(output,r-l+1)
+        else:
+            """
+            There are two cases if s[r] in seen:
+            case1: s[r] is inside the current window, we need to change the window by moving left pointer to seen[s[r]] + 1.
+            case2: s[r] is not inside the current window, we can keep increase the window
+            """
+            if seen[s[r]] < l:
+                output = max(output,r-l+1)
+            else:
+                l = seen[s[r]] + 1
+        seen[s[r]] = r
+    
+    return output
+
 
 case1 = "abcabcbb"
 case2 = "bbbbb"
