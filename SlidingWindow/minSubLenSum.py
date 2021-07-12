066be5a27503aafda2c2524c780f75c427958cd4
Andrew Park <cpark128@umd.edu>
2021-07-12 14:10:06 -0400

    6/25

--- SlidingWindow/minSubLenSum.py
+++ SlidingWindow/minSubLenSum.py
@@ -1,3 +1,12 @@
+"""
+LC 209. Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, 
+return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] 
+of which the sum is greater than or equal to target. 
+If there is no such subarray, return 0 instead.
+"""
+
 # Dynamic window size problem
 def smallest_subarray_with_given_sum(s, arr):
     (start, currSum, currLen, minLen) = (0, 0, 0, float('inf'))
