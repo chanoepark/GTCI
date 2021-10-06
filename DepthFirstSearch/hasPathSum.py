92c6a2be73d0caed16a64a52c639923acdbff9ad
Andrew Park <cpark128@umd.edu>
2021-10-06 14:36:01 -0400

    10/6

---
+++ DepthFirstSearch/hasPathSum.py
@@ -0,0 +1,45 @@
+"""
+LC 112. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+
+Example 3:
+Input: root = [1,2], targetSum = 0
+Output: false
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def hasPathSum(root, targetSum):
+    if not root:
+        return False
+    elif targetSum - root.val == 0:
+        return True
+    return hasPathSum(root.left, targetSum - root.val) or \
+            hasPathSum(root.right, targetSum - root.val)
+
+
+root = TreeNode(12)
+root.left = TreeNode(7)
+root.right = TreeNode(1)
+root.left.left = TreeNode(9)
+root.right.left = TreeNode(10)
+root.right.right = TreeNode(5)
+print(f'Tree has path: {str(hasPathSum(root, 23))}')
+print(f'Tree has path: {str(hasPathSum(root, 16))}')
+
