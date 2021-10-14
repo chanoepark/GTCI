bea192a277e278f83487682a380ad1e46943df28
Andrew Park <cpark128@umd.edu>
2021-10-14 01:21:41 -0400

    10/14

---
+++ DepthFirstSearch/pathSum.py
@@ -0,0 +1,62 @@
+"""
+LC 113. Path Sum II
+
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: []
+
+Example 3:
+Input: root = [1,2], targetSum = 0
+Output: []
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def pathSum(root, targetSum):
+    paths = []
+    pathSumHelper(root, targetSum, [], paths)
+    return paths
+
+
+def pathSumHelper(root, targetSum, path, paths):
+    if not root:
+        return
+    
+    path.append(root.val)  # Add current node to path
+    
+    if targetSum == root.val and not root.left and not root.right:
+        # Leaf node that meets the targetSum
+        paths.append(path.copy())
+    else:
+        pathSumHelper(root.left, targetSum - root.val, path, paths)
+        pathSumHelper(root.right, targetSum - root.val, path, paths)
+
+    # Consulted solution: Need to remove the current node from the path list to backtrack.
+    path.pop()
+
+
+root = TreeNode(12)
+root.left = TreeNode(7)
+root.right = TreeNode(1)
+root.left.left = TreeNode(4)
+root.right.left = TreeNode(10)
+root.right.right = TreeNode(5)
+targetSum = 23
+print(f'Tree paths with sum {targetSum}: {str(pathSum(root, targetSum))}')
+
